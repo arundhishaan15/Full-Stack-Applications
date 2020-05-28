0fea548278e3f180fe4fac5c416cd8542c6fe091
arundhishaan15 <arundhishaan@gmail.com>
2020-05-27 17:55:08 -0700

    styling maps!!

--- Google Maps Store Locator/js/index.js
+++ Google Maps Store Locator/js/index.js
@@ -1,17 +1,134 @@
 var map;
-
+var markers = [];
 var infoWindow;
-
 function initMap() {
+    var styledMapType = new google.maps.StyledMapType(
+        [
+          {elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},
+          {elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
+          {elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
+          {
+            featureType: 'administrative',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#c9b2a6'}]
+          },
+          {
+            featureType: 'administrative.land_parcel',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#dcd2be'}]
+          },
+          {
+            featureType: 'administrative.land_parcel',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#ae9e90'}]
+          },
+          {
+            featureType: 'landscape.natural',
+            elementType: 'geometry',
+            stylers: [{color: '#dfd2ae'}]
+          },
+          {
+            featureType: 'poi',
+            elementType: 'geometry',
+            stylers: [{color: '#dfd2ae'}]
+          },
+          {
+            featureType: 'poi',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#93817c'}]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'geometry.fill',
+            stylers: [{color: '#a5b076'}]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#447530'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{color: '#f5f1e6'}]
+          },
+          {
+            featureType: 'road.arterial',
+            elementType: 'geometry',
+            stylers: [{color: '#fdfcf8'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{color: '#f8c967'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#e9bc62'}]
+          },
+          {
+            featureType: 'road.highway.controlled_access',
+            elementType: 'geometry',
+            stylers: [{color: '#e98d58'}]
+          },
+          {
+            featureType: 'road.highway.controlled_access',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#db8555'}]
+          },
+          {
+            featureType: 'road.local',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#806b63'}]
+          },
+          {
+            featureType: 'transit.line',
+            elementType: 'geometry',
+            stylers: [{color: '#dfd2ae'}]
+          },
+          {
+            featureType: 'transit.line',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#8f7d77'}]
+          },
+          {
+            featureType: 'transit.line',
+            elementType: 'labels.text.stroke',
+            stylers: [{color: '#ebe3cd'}]
+          },
+          {
+            featureType: 'transit.station',
+            elementType: 'geometry',
+            stylers: [{color: '#dfd2ae'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'geometry.fill',
+            stylers: [{color: '#b9d3c2'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#92998d'}]
+          }
+        ],
+        {name: 'Styled Map'});
     var losAngeles = {
         lat: 34.0522,
         lng: -118.2437
     }
     map = new google.maps.Map(document.getElementById('map'), {
         center: losAngeles,
-        zoom: 10
+        zoom: 10,
+        mapTypeControlOptions: {
+            mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+                    'styled_map']
+          }
     });
-    infoWindow = 
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
+    infoWindow = new google.maps.InfoWindow();
     displayStores();
     showStoresMarkers();
 }
@@ -19,11 +136,11 @@ function initMap() {
 function displayStores() {
 
     var storesHtml = "";
-    stores.array.forEach(function(store, index) {
+    stores.forEach(function(store, index) {
         var address = store.addressLines;
         var phone = store.phoneNumber;
         storesHtml += ` 
-            <div class="store-container">
+        <div class="store-container">
             <div class="store-info-container">
                 <div class="store-address">
                     <span> ${address[0]} </span>
@@ -41,19 +158,28 @@ function displayStores() {
 }
 
 function showStoresMarkers() {
-    var bounds = new google.maps.jhfdsflkdshfljadfhadskjfhasjskdfhdfbskdfbakdfkjfb
+    var bounds = new google.maps.LatLngBounds();
     stores.forEach(function(store,index) {
         var latlng = new google.maps.LatLng(
             store.coordinates.latitude,
             store.coordinates.longitude);
-            var name = store.name;
-            var address = store.addressLines[0];
-            bounds.extend(latlng);
-            createMarker(latlng, name, address) ;
+        var name = store.name;
+        var address = store.addressLines[0];
+        bounds.extend(latlng);
+        createMarker(latlng, name, address) ;
     })
     map.fitBounds(bounds);
 }
 
 function createMarker(latlng, name, address) {
-
+    var html = "<b>" + name + "</b> <br/>" + address;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: latlng
+    });
+    google.maps.event.addListener(marker, 'mouseover', function() {
+      infoWindow.setContent(html);
+      infoWindow.open(map, marker);
+    });
+    markers.push(marker);
 }
