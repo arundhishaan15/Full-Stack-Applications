f9a5976fb5e99dec42a352c45de47a609aa14bca
arundhishaan15 <arundhishaan@gmail.com>
2020-06-14 22:06:30 -0700

    Add text functionality completed!!

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,32 +1,54 @@
-import React from 'react';
-import './App.css';
-import Todo from './todo'
+import React, { useState } from "react";
+import "./App.css";
+import Todo from "./todo";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+import { faPlusSquare } from "@fortawesome/free-solid-svg-icons";
 
-function App() {    
+function App() {
   // Creating a list of Todos
-  // const [todos, setTodos] = useState(initialState)
-  
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
   return (
     <div className="todo">
-      
       <div className="title-container">
         <h1 id="appTitle">Todo App</h1>
       </div>
 
-      <div className="input-container">
-        <input id="textInput" type="text" placeholder="Tell me..."></input>
-        <FontAwesomeIcon icon={faPlusSquare} size="2x" style={{ color: 'blue' }} onClick="this.textAdd()" />
+      <div>
+        <form className="input-container">
+
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            id="textInput"
+            type="text"
+            placeholder="Tell me..."
+          ></input>
+
+          <button id="addButton" type="submit" onClick={handleSubmit} disabled={!input}>
+            <FontAwesomeIcon
+              icon={faPlusSquare}
+              size="3x"
+              style={{ color: "blue" }}
+            />
+          </button>
+
+        </form>
       </div>
 
-      <Todo text="Okay" />
-      <Todo text="Okay" />
-      <Todo text="Okay" />
-      <Todo text="Okay" />
-      
-    </div>
+      {todos.map((todo) => (
+        <Todo text={todo} />
+      ))}
 
+    </div>
   );
 }
 
